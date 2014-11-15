2b107dd9db531637b50bd4a72dacda6670ab71a6
Alfredo Barrón Rodríguez <alfreedobarron@gmail.com>
2014-11-15 16:01:30 -0600

    base de datos

--- conexion.php
+++ conexion.php
@@ -1,8 +1,17 @@
 <?php
+  if(getenv('DATABASE_URL') != false){
+  $dbopts = parse_url(getenv('DATABASE_URL'));
+  $dbname = ltrim($dbopts['path'],'/');
+  $host = $dbopts['host'];
+  $port = $dbopts['port'];
+  $user = $dbopts['user'];
+  $pass = $dbopts['pass'];
+  } else {
 	$host="localhost";
 	$user="postgres";
 	$pass="";
 	$dbname='carritocompras';
+  }
   $conn = pg_connect("host=".$host." user=".$user." dbname=".$dbname." password=".$pass);
   if (!$conn)
   {
